0a6f279b4386880cab8cc049fb9e867db8afbcd9
Eric Shim <hshim30@gatech.edu>
2017-10-29 04:28:37 -0400

    Add location listener.

--- app/src/main/java/appathon17/driversleep/MainActivity.java
+++ app/src/main/java/appathon17/driversleep/MainActivity.java
@@ -1,24 +1,29 @@
 package appathon17.driversleep;
 
+import static com.google.android.gms.vision.face.FaceDetector.ALL_CLASSIFICATIONS;
+import static com.google.android.gms.vision.face.FaceDetector.ALL_LANDMARKS;
+
 import android.Manifest;
-import android.app.Activity;
+import android.Manifest.permission;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
-import appathon17.driversleep.database.DbOpenHelper;
-import appathon17.driversleep.logging.Logger;
 import appathon17.driversleep.ui.CameraSourcePreview;
 import appathon17.driversleep.ui.GraphicOverlay;
 import com.google.android.gms.common.ConnectionResult;
@@ -30,24 +35,31 @@ import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 import java.io.IOException;
 
-
-import static com.google.android.gms.vision.face.FaceDetector.ALL_CLASSIFICATIONS;
-import static com.google.android.gms.vision.face.FaceDetector.ALL_LANDMARKS;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
   private static final String TAG = MainActivity.class.getSimpleName();
 
   private static final int RC_HANDLE_GMS = 9001;
   // permission request codes need to be < 256
-  private static final int RC_HANDLE_CAMERA_PERM = 2;
+  private static final int PERM_REQ_CODE_CAMERA = 2;
+  private static final int PERM_REQ_CODE_LOCATION = 3;
+  private static final int PERM_REQ_CODE_ALL = 77;
+
+  private static final String[] REQUIRED_PERMISSIONS = new String[]{
+      permission.CAMERA,
+      permission.ACCESS_FINE_LOCATION
+  };
+
   private CameraSource mCameraSource = null;
   private CameraSourcePreview mPreview;
   private GraphicOverlay mGraphicOverlay;
   private MediaPlayer mp;
-  private FaceTrackerCallback callback;
 
-  // Snippet
-  private DbOpenHelper dbOpenHelper;
+  private LocationManager mLocationManager;
+  private Location mLocation;
+
+  private static final long LOCATION_UPDATE_MIN_INTERVAL = 300;
+  private static final float LOCATION_UPDATE_MIN_DIST = 5;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -56,52 +68,106 @@ public class MainActivity extends AppCompatActivity {
 
     mPreview = findViewById(R.id.preview);
     mGraphicOverlay = findViewById(R.id.faceOverlay);
-    // Check for the camera permission before accessing the camera.  If the
-    // permission is not granted yet, request permission.
-    int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-    if (rc == PackageManager.PERMISSION_GRANTED) {
-      createCameraSource();
-    } else {
-      requestCameraPermission();
-    }
 
-    // Snippet
-    dbOpenHelper = new DbOpenHelper(this);
-    Logger logger = new Logger(dbOpenHelper.getWritableDatabase(), 1);
-    logger.begin();
-    logger.log("Seriwjerowiejr", 01294.214090, 0.2492094);
+    // Check all required permissions first!
+    if (!checkAllPerms()) {
+      allPermissionsCheckedAction();
+    } else {
+      requestAllPermissions();
+    }
   }
 
-  /**
-   * Handles the requesting of the camera permission.  This includes
-   * showing a "Snackbar" message of why the permission is needed then
-   * sending the request.
-   */
-  private void requestCameraPermission() {
-    Log.w(TAG, "Camera permission is not granted. Requesting permission");
+  private void allPermissionsCheckedAction() {
+    createCameraSource();
+  }
 
-    final String[] permissions = new String[]{Manifest.permission.CAMERA};
+  private void requestAllPermissions() {
+    ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, PERM_REQ_CODE_ALL);
+  }
 
-    if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
-        Manifest.permission.CAMERA)) {
-      ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+  private boolean checkAllPerms() {
+    for (String perm : REQUIRED_PERMISSIONS) {
+      if (!checkPerm(perm)) {
+        Log.w(TAG, perm + ": permission not granted.");
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private boolean checkPerm(String perm) {
+    return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        == PackageManager.PERMISSION_GRANTED;
+  }
+
+  @SuppressWarnings({"MissingPermission"})
+  private void initLocation() {
+    mLocationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+    if (mLocationManager == null) {
+      Log.e(TAG, "LocationManager turned out to be null!");
       return;
     }
 
-    final Activity thisActivity = this;
+    // getting GPS status
+    boolean isGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-    View.OnClickListener listener = new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        ActivityCompat.requestPermissions(thisActivity, permissions,
-            RC_HANDLE_CAMERA_PERM);
-      }
-    };
+    if (!isGPSEnabled) {
+      Log.w(TAG, "GPS not enabled on device.");
+      AlertDialog.Builder builder = new AlertDialog.Builder(this);
+      builder.setMessage(R.string.enable_gps)
+          .setPositiveButton("Ok", new OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+              // FIRE ZE MISSILES!
+            }
+          })
+          .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+              // User cancelled the dialog
+            }
+          });
+      builder.create().show();
+    }
 
-    Snackbar.make(mGraphicOverlay, R.string.permission_camera_rationale,
-        Snackbar.LENGTH_INDEFINITE)
-        .setAction(R.string.ok, listener)
-        .show();
+    mLocationManager.requestLocationUpdates(
+        LocationManager.GPS_PROVIDER,
+        LOCATION_UPDATE_MIN_INTERVAL,
+        LOCATION_UPDATE_MIN_DIST, this);
+
+    mLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+  }
+
+  @Override
+  public void onLocationChanged(Location location) {
+    mLocation = location;
+  }
+
+  @Override
+  public void onStatusChanged(String s, int i, Bundle bundle) {
+
+  }
+
+  @Override
+  public void onProviderEnabled(String s) {
+
+  }
+
+  @Override
+  public void onProviderDisabled(String s) {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setMessage(R.string.enable_gps)
+        .setPositiveButton("Ok", new OnClickListener() {
+          public void onClick(DialogInterface dialog, int id) {
+            // FIRE ZE MISSILES!
+          }
+        })
+        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int id) {
+            // User cancelled the dialog
+          }
+        });
+    builder.create().show();
   }
 
   interface FaceTrackerCallback {
@@ -121,16 +187,11 @@ public class MainActivity extends AppCompatActivity {
     // each face on screen.  The factory is used by the multi-processor to create a separate
     // tracker instance for each face.
     FaceDetector faceDetector = new FaceDetector.Builder(context)
-            .setProminentFaceOnly(true)
-            .setLandmarkType(ALL_LANDMARKS) // Get all landmarks (track eyes)
-            .setClassificationType(ALL_CLASSIFICATIONS) // Allow probabilities to be computed
-            .build();
-    callback = new FaceTrackerCallback() {
-      @Override
-      public void onUpdate(Face item) {
+        .setProminentFaceOnly(true)
+        .setLandmarkType(ALL_LANDMARKS) // Get all landmarks (track eyes)
+        .setClassificationType(ALL_CLASSIFICATIONS) // Allow probabilities to be computed
+        .build();
 
-      }
-    };
     mp = MediaPlayer.create(context, R.raw.buzzer);
     FaceTrackerFactory faceFactory = new FaceTrackerFactory(mGraphicOverlay, mp);
     faceDetector.setProcessor(
@@ -227,34 +288,29 @@ public class MainActivity extends AppCompatActivity {
    * @see #requestPermissions(String[], int)
    */
   @Override
-  public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-    if (requestCode != RC_HANDLE_CAMERA_PERM) {
-      Log.d(TAG, "Got unexpected permission result: " + requestCode);
-      super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-      return;
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+      @NonNull int[] grantResults) {
+    switch (requestCode) {
+      case PERM_REQ_CODE_ALL:
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+          boolean good = true;
+          for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+              good = false;
+              break;
+            }
+          }
+          if (!good) {
+            Log.e(TAG, "Not all required permissions granted. Requesting again.");
+            requestAllPermissions();
+          }
+        }
+        break;
+      case PERM_REQ_CODE_CAMERA:
+        break;
+      case PERM_REQ_CODE_LOCATION:
+        break;
     }
-
-    if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-      Log.d(TAG, "Camera permission granted - initialize the camera source");
-      // we have permission, so create the camerasource
-      createCameraSource();
-      return;
-    }
-
-    Log.e(TAG, "Permission not granted: results len = " + grantResults.length +
-        " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
-
-    DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
-      public void onClick(DialogInterface dialog, int id) {
-        finish();
-      }
-    };
-
-    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    builder.setTitle("Multitracker sample")
-        .setMessage(R.string.no_camera_permission)
-        .setPositiveButton(R.string.ok, listener)
-        .show();
   }
   /**
    * Starts or restarts the camera source, if it exists.  If the camera source doesn't exist yet
