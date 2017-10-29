dbd16a450f25d9aca7550b88af09af2a37bc53e4
Eric Shim <hshim30@gatech.edu>
2017-10-28 20:10:50 -0400

    First commit.

---
+++ app/src/main/java/appathon17/driversleep/MainActivity.java
@@ -0,0 +1,13 @@
+package appathon17.driversleep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
